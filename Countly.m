a9127fea1950f26ae56c09469e735ab0da5ecb28
Séraphin Huguenot <serahug@goprod.fr>
2013-04-28 22:32:09 +0200

    Adding update interval setting.

--- Countly.m
+++ Countly.m
@@ -635,6 +635,7 @@ static Countly *s_sharedCountly = nil;
 {
 	if (self = [super init])
 	{
+        updateInterval = 30.0;
 		timer = nil;
 		isSuspended = NO;
 		unsentSessionLength = 0;
@@ -658,9 +659,14 @@ static Countly *s_sharedCountly = nil;
 	return self;
 }
 
+- (void)setUpdateInterval:(NSTimeInterval)interval
+{
+    updateInterval = interval;
+}
+
 - (void)start:(NSString *)appKey withHost:(NSString *)appHost
 {
-	timer = [NSTimer scheduledTimerWithTimeInterval:30.0
+	timer = [NSTimer scheduledTimerWithTimeInterval:updateInterval
 											 target:self
 										   selector:@selector(onTimer:)
 										   userInfo:nil
