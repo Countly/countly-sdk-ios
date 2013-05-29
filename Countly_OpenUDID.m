a260b86ba482073bc215bbf981bf80263e7b4fe1
David Beck <david@davidbeck.co>
2013-05-29 09:01:45 -0700

    Fixed memory leak
    Because it has "create" in the function name, it needs to be released.

--- Countly_OpenUDID.m
+++ Countly_OpenUDID.m
@@ -187,7 +187,9 @@ static int const kOpenUDIDRedundancySlots = 100;
     {
       // generate a new uuid and store it in user defaults
       CFUUIDRef uuid = CFUUIDCreate(NULL);
-      appUID = (NSString *) CFUUIDCreateString(NULL, uuid);
+      CFString uuidString = CFUUIDCreateString(NULL, uuid);
+      appUID = (NSString *) uuidString;
+      CFRelease(uuidString);
       CFRelease(uuid);
     }
   
