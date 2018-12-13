e3a53dbfc2f48418f0976e3e4e331018e25fca7a
erkanyildiz <ey@count.ly>
2018-12-13 23:50:23 +0900

    Fixed openURL freeze caused by iOS

--- CountlyPushNotifications.m
+++ CountlyPushNotifications.m
@@ -352,7 +352,7 @@ NSString* const kCountlyTokenError = @"kCountlyTokenError";
     if (!URLString)
         return;
 
-    dispatch_async(dispatch_get_main_queue(), ^
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^
     {
 #if TARGET_OS_IOS
         [UIApplication.sharedApplication openURL:[NSURL URLWithString:URLString]];
