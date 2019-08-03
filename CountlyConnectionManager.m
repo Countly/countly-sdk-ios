97156512a81c8c86c8db18de676081c0174e9792
erkanyildiz <ey@count.ly>
2019-08-03 23:04:44 +0900

    Fixed immediate crash report request for temporary device ID mode

--- CountlyConnectionManager.m
+++ CountlyConnectionManager.m
@@ -323,7 +323,7 @@ const NSInteger kCountlyGETRequestMaxLength = 2048;
     NSString* queryString = [[self queryEssentials] stringByAppendingFormat:@"&%@=%@",
                              kCountlyQSKeyCrash, report];
 
-    if (!immediately || CountlyDeviceInfo.sharedInstance.isDeviceIDTemporary)
+    if (!immediately)
     {
         [CountlyPersistency.sharedInstance addToQueue:queryString];
         [self proceedOnQueue];
@@ -341,6 +341,15 @@ const NSInteger kCountlyGETRequestMaxLength = 2048;
     if (self.customHeaderFieldName && !self.customHeaderFieldValue)
     {
         COUNTLY_LOG(@"customHeaderFieldName specified on config, but customHeaderFieldValue not set! Crash report stored to be sent later!");
+
+        [CountlyPersistency.sharedInstance addToQueue:queryString];
+        [CountlyPersistency.sharedInstance saveToFileSync];
+        return;
+    }
+
+    if (CountlyDeviceInfo.sharedInstance.isDeviceIDTemporary)
+    {
+        COUNTLY_LOG(@"Device ID is set as CLYTemporaryDeviceID! Crash report stored to be sent later!");
 
         [CountlyPersistency.sharedInstance addToQueue:queryString];
         [CountlyPersistency.sharedInstance saveToFileSync];
