2b19c35f9e79447e68b83880e1f06545172848c1
Andrew McKnight <amcknight@rightpoint.com>
2019-10-30 12:00:35 -0800

    fix: add checks for countly dsym script parameters
    - add usage help output for bad invocations
    - update header comment to include instructions for invoking
    script when delivered via CocoaPods

--- countly_dsym_uploader.sh
+++ countly_dsym_uploader.sh
@@ -6,14 +6,27 @@
 
 
 # For your target, go to Build Phases tab and choose New Run Script Phase after clicking plus (+) button.
-# Add these two lines and do not forget to replace YOUR_COUNTLY_SERVER and YOUR_APP_KEY.
+# Add this line and do not forget to replace YOUR_COUNTLY_SERVER and YOUR_APP_KEY.
 #
 # COUNTLY_DSYM_UPLOADER=$(/usr/bin/find $SRCROOT -name "countly_dsym_uploader.sh" | head -n 1)
 # sh "$COUNTLY_DSYM_UPLOADER" "https://YOUR_COUNTLY_SERVER" "YOUR_APP_KEY"
+#
+# or if you're using CocoaPods:
+# sh "$(PODS_ROOT)/Countly/countly_dsym_uploader.sh" "https://YOUR_COUNTLY_SERVER" "YOUR_APP_KEY"
 
+HOST="$1";
+APPKEY="$2";
 
 # Common functions
 countly_log () { echo "[Countly] $1"; }
+
+countly_usage () { 
+	countly_log
+	countly_log "You must invoke the script as follows:"
+	countly_log
+	countly_log "	sh \"/path/to/.../countly_dsym_uploader.sh\" \"https://YOUR_COUNTLY_SERVER\" \"YOUR_APP_KEY\" [\"/path/to/.../your.dSYM\"]"
+	countly_log
+}
 
 countly_fail () { countly_log "$1"; exit 0; }
 
@@ -27,8 +40,18 @@ if [ ! "$DWARF_DSYM_FOLDER_PATH" ] || [ ! "$DWARF_DSYM_FILE_NAME" ]; then
     countly_fail "Xcode Environment Variables are missing!"
 fi
 
+if [[ -z $HOST ]]; then
+	countly_usage
+	countly_fail "Did not provide a Count.ly host to which to upload dSYMs."
+fi
+
+if [[ -z $APPKEY ]]; then
+	countly_usage
+	countly_fail "Did not provide an App Key for your dSYMs."
+fi
+
 DSYM_PATH="${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}";
-if [ ! -d $DSYM_PATH ]; then
+if [ ! -d "${DSYM_PATH}" ]; then
     countly_fail "$DWARF_DSYM_FILE_NAME does not exist!"
 fi
 
@@ -55,8 +78,6 @@ fi
 
 
 # Preparing for upload
-HOST="$1";
-APPKEY="$2";
 ENDPOINT="/i/crash_symbols/upload_symbol"
 QUERY="?platform=ios&app_key=${APPKEY}&build=${BUILD_UUIDS}"
 URL="$HOST$ENDPOINT${QUERY}"
