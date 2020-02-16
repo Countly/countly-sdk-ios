f02803ad648d9a5025078a0a8027f1a17f0c7e51
erkanyildiz <ey@count.ly>
2020-02-16 14:27:09 +0900

    Updated orientation metric for watchOS

--- CountlyDeviceInfo.m
+++ CountlyDeviceInfo.m
@@ -400,6 +400,15 @@ NSString* const kCountlyMetricKeyInstalledWatchApp  = @"_installed_watch_app";
 #if TARGET_OS_IOS
     NSArray *orientations = @[@"Unknown", @"Portrait", @"PortraitUpsideDown", @"LandscapeLeft", @"LandscapeRight", @"FaceUp", @"FaceDown"];
     return orientations[UIDevice.currentDevice.orientation];
+#elif TARGET_OS_WATCH
+    if (@available(watchOS 3.0, *))
+    {
+        NSArray *orientations = @[@"CrownLeft", @"CrownRight"];
+        WKInterfaceDeviceCrownOrientation orientation = WKInterfaceDevice.currentDevice.crownOrientation;
+        if (orientation >= 0 && orientation < orientations.count)
+            return orientations[orientation];
+    }
+    return nil;
 #else
     return nil;
 #endif
