93fac25a69c545c7e9248cbddd55d069d5231c32
erkanyildiz <ey@count.ly>
2022-03-31 16:35:05 +0900

    Updated SDK version strings to 21.11.2

--- CountlyCommon.m
+++ CountlyCommon.m
@@ -26,7 +26,7 @@ NSString* const kCountlyOrientationKeyMode = @"mode";
 #endif
 @end
 
-NSString* const kCountlySDKVersion = @"21.11.1";
+NSString* const kCountlySDKVersion = @"21.11.2";
 NSString* const kCountlySDKName = @"objc-native-ios";
 
 NSString* const kCountlyErrorDomain = @"ly.count.ErrorDomain";
