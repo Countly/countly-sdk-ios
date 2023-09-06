c40e32f991c697fac84760deec5ef68e65a2cc25
Artūrs Kadiķis <kadikis.arturs@gmail.com>
2023-09-06 12:46:38 +0300

    Update CountlyConfig.h

--- CountlyConfig.h
+++ CountlyConfig.h
@@ -577,7 +577,7 @@ typedef enum : NSUInteger
 
 /**
  * For enabling automatic AB enrolling on download.
- * @discussion If set, AB will automatically enroll on downloanding values.
+ * @discussion If set, user will will automatically be enrolled in AB test when downloading RC values.
  */
 @property (nonatomic) BOOL enrollABOnRCDownload;
 
