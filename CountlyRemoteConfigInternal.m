462f834fa32d5a59c3388dac833a70bebf7c412c
Arif Burak Demiray <burakdemiray09@hotmail.com>
2024-06-27 13:24:29 +0300

    feat: add caching removing

--- CountlyRemoteConfigInternal.m
+++ CountlyRemoteConfigInternal.m
@@ -635,6 +635,7 @@ CLYRequestResult const CLYResponseError         = @"CLYResponseError";
             return;
         }
         
+        [self clearCachedRemoteConfig];
         [self downloadRemoteConfigAutomatically];
         
         CLY_LOG_D(@"Enroll RC Variant Request <%p> successfully completed.", request);
