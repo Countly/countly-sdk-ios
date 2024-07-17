1e758ee67a48884d15a998b258c1cd7165f0ae56
turtledreams <deniztaygunerten@gmail.com>
2024-07-17 23:51:28 +0900

    has include

--- CountlyCrashReporter.m
+++ CountlyCrashReporter.m
@@ -8,7 +8,7 @@
 #import <mach-o/dyld.h>
 #include <execinfo.h>
 
-#if __has_include(<CrashReporter/CrashReporter.h>)
+#if defined(__has_include) && __has_include(<CrashReporter/CrashReporter.h>)
     #define COUNTLY_PLCRASHREPORTER_EXISTS true
     #import <CrashReporter/CrashReporter.h>
 #else
