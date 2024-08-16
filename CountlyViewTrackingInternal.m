43c018f228b834baa882257f25ceaa33732a3898
ijunaid <mjunaid.akram@yahoo.com>
2024-08-16 10:46:41 +0500

    No need to send old view segmentation when starting a view again

--- CountlyViewTrackingInternal.m
+++ CountlyViewTrackingInternal.m
@@ -542,12 +542,6 @@ NSString* const kCountlyVTKeyDur      = @"dur";
         {
             NSString *viewID = [self startViewInternal:viewData.viewName customSegmentation:nil isAutoStoppedView:viewData.isAutoStoppedView];
             
-            // Retrieve the newly created viewData for the viewID
-            CountlyViewData* viewDataNew = self.viewDataDictionary[viewID];
-            
-            // Copy the segmentation data from the old view to the new view
-            viewDataNew.segmentation = viewData.segmentation.mutableCopy;
-            
             // Add the old view's ID to the array for removal later
             [keysToRemove addObject:viewData.viewID];
         }
