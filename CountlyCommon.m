d0c54f8b33b5cc0f4da311e1c7e19ade99572d2b
turtledreams <62231246+turtledreams@users.noreply.github.com>
2024-12-03 23:19:10 +0900

    Version 7.9

--- CountlyCommon.m
+++ CountlyCommon.m
@@ -29,7 +29,7 @@ NSString* const kCountlyVisibility = @"cly_v";
 #endif
 @end
 
-NSString* const kCountlySDKVersion = @"24.7.8";
+NSString* const kCountlySDKVersion = @"24.7.9";
 NSString* const kCountlySDKName = @"objc-native-ios";
 
 NSString* const kCountlyErrorDomain = @"ly.count.ErrorDomain";
