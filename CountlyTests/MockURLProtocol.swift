4a8753fe63a033084ce887ca5d27bbe1dcf8bbc1
Arif Burak Demiray <burakdemiray09@hotmail.com>
2025-04-02 16:51:48 +0300

    feat: test helper url protocol mock

---
+++ CountlyTests/MockURLProtocol.swift
@@ -0,0 +1,45 @@
+//
+//  MockURLProtocol.swift
+//  Countly
+//
+//  Created by Arif Burak Demiray on 27.03.2025.
+//  Copyright © 2025 Countly. All rights reserved.
+//
+
+
+import Foundation
+import XCTest
+
+class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) -> (Data?, URLResponse?, Error?))?
+
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            fatalError("Request handler not set!")
+        }
+        
+        let (data, response, error) = handler(request)
+        
+        if let response = response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        if let data = data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        if let error = error {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+
+        client?.urlProtocolDidFinishLoading(self)
+    }
+
+    override func stopLoading() {}
+}
