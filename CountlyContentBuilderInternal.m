89218f38c7490527feb0857ac8368fc209c0044a
Arif Burak Demiray <burakdemiray09@hotmail.com>
2025-04-16 12:57:21 +0300

    feat: revert formatting for CountlyContentBuilderInternal

--- CountlyContentBuilderInternal.m
+++ CountlyContentBuilderInternal.m
@@ -6,9 +6,9 @@
 #import "CountlyContentBuilderInternal.h"
 #import "CountlyWebViewManager.h"
 
-// TODO: improve logging, check edge cases
-NSString *const kCountlyEndpointContent = @"/o/sdk/content";
-NSString *const kCountlyCBFetchContent = @"queue";
+//TODO: improve logging, check edge cases
+NSString* const kCountlyEndpointContent = @"/o/sdk/content";
+NSString* const kCountlyCBFetchContent  = @"queue";
 
 @implementation CountlyContentBuilderInternal {
     BOOL _isRequestQueueLocked;
@@ -20,8 +20,7 @@ NSString *const kCountlyCBFetchContent = @"queue";
 NSInteger const contentInitialDelay = 4;
 
 #if (TARGET_OS_IOS)
-+ (instancetype)sharedInstance
-{
++ (instancetype)sharedInstance {
     static CountlyContentBuilderInternal *instance = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
@@ -38,76 +37,69 @@ NSInteger const contentInitialDelay = 4;
         _requestTimer = nil;
         _isCurrentlyContentShown = NO;
     }
-
+    
     return self;
 }
 
-- (void)enterContentZone
-{
-
-    if (_isCurrentlyContentShown)
-    {
-        CLY_LOG_I(@"%s, a content is already shown, skipping", __FUNCTION__);
+- (void)enterContentZone {
+    
+    if(_isCurrentlyContentShown){
+        CLY_LOG_I(@"%s, a content is already shown, skipping" ,__FUNCTION__);
     }
-
+    
     [self enterContentZone:@[]];
 }
 
-- (void)enterContentZone:(NSArray<NSString *> *)tags
-{
+- (void)enterContentZone:(NSArray<NSString *> *)tags {
     [_minuteTimer invalidate];
     _minuteTimer = nil;
-
+    
     if (!CountlyConsentManager.sharedInstance.consentForContent)
         return;
-
-    if (_requestTimer != nil)
-    {
+    
+    if(_requestTimer != nil) {
         CLY_LOG_I(@"%s, Already entered for content zone, please exit from content zone first to start again", __FUNCTION__);
         return;
     }
-
+    
     self.currentTags = tags;
     int contentDelay = 0;
-
-    if (CountlyCommon.sharedInstance.timeSinceLaunch < contentInitialDelay)
-    {
+    
+    if (CountlyCommon.sharedInstance.timeSinceLaunch < contentInitialDelay) {
         contentDelay = contentInitialDelay;
     }
-
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(contentDelay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self fetchContents];
-        self->_requestTimer = [NSTimer scheduledTimerWithTimeInterval:self->_zoneTimerInterval target:self selector:@selector(fetchContents) userInfo:nil repeats:YES];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(contentDelay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^
+    {
+        [self fetchContents];;
+        self->_requestTimer = [NSTimer scheduledTimerWithTimeInterval:self->_zoneTimerInterval
+                                                         target:self
+                                                       selector:@selector(fetchContents)
+                                                       userInfo:nil
+                                                        repeats:YES];
     });
 }
 
-- (void)exitContentZone
-{
+- (void)exitContentZone {
     [self clearContentState];
 }
 
-- (void)changeContent:(NSArray<NSString *> *)tags
-{
-    if (![tags isEqualToArray:self.currentTags])
-    {
+- (void)changeContent:(NSArray<NSString *> *)tags {
+    if (![tags isEqualToArray:self.currentTags]) {
         [self exitContentZone];
         [self enterContentZone:tags];
     }
 }
 
-- (void)refreshContentZone
-{
-
+- (void)refreshContentZone {
     if (![CountlyServerConfig.sharedInstance refreshContentZoneEnabled])
     {
         return;
     }
-
-    if (_isCurrentlyContentShown)
-    {
-        CLY_LOG_I(@"%s, a content is already shown, skipping", __FUNCTION__);
+    if(_isCurrentlyContentShown){
+        CLY_LOG_I(@"%s, a content is already shown, skipping" ,__FUNCTION__);
     }
-
+    
     [self exitContentZone];
     [CountlyConnectionManager.sharedInstance attemptToSendStoredRequests];
     
@@ -118,69 +110,60 @@ NSInteger const contentInitialDelay = 4;
     
 }
 
-    #pragma mark - Private Methods
+#pragma mark - Private Methods
 
-- (void)clearContentState
-{
+- (void)clearContentState {
     [_requestTimer invalidate];
     _requestTimer = nil;
-
+    
     [_minuteTimer invalidate];
     _minuteTimer = nil;
     self.currentTags = nil;
     _isRequestQueueLocked = NO;
 }
 
-- (void)fetchContents
-{
+- (void)fetchContents {
     if (!CountlyConsentManager.sharedInstance.consentForContent)
         return;
 
     if (!CountlyServerConfig.sharedInstance.networkingEnabled)
         return;
-
-    if (_isRequestQueueLocked)
-    {
+    if  (_isRequestQueueLocked) {
         return;
     }
-
+    
     _isRequestQueueLocked = YES;
-
-    id handler = ^(NSData *data, NSURLResponse *response, NSError *error) {
-        if (error)
-        {
+    
+    NSURLSessionTask *dataTask = [[NSURLSession sharedSession] dataTaskWithRequest:[self fetchContentsRequest] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error) {
             CLY_LOG_I(@"%s, Fetch content details failed: %@", __FUNCTION__, error);
             self->_isRequestQueueLocked = NO;
             return;
         }
-
+        
         NSError *jsonError;
         NSDictionary *jsonResponse = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
-
-        if (jsonError)
-        {
+        
+        if (jsonError) {
             CLY_LOG_I(@"%s, Failed to parse JSON: %@", __FUNCTION__, jsonError);
             self->_isRequestQueueLocked = NO;
             return;
         }
-
-        if (!jsonResponse)
-        {
+        
+        if (!jsonResponse) {
             CLY_LOG_I(@"%s, Received empty or null response.", __FUNCTION__);
             self->_isRequestQueueLocked = NO;
             return;
         }
-
+        
         NSString *pathToHtml = jsonResponse[@"html"];
         NSDictionary *placementCoordinates = jsonResponse[@"geo"];
-        if (pathToHtml)
-        {
+        if(pathToHtml) {
             [self showContentWithHtmlPath:pathToHtml placementCoordinates:placementCoordinates];
         }
         self->_isRequestQueueLocked = NO;
-    };
-
-    NSURLSessionTask *dataTask = [CountlyCommon.sharedInstance.URLSession dataTaskWithRequest:[self fetchContentsRequest] completionHandler:handler];
+    }];
+    
     [dataTask resume];
 }
 
@@ -208,46 +191,37 @@ NSInteger const contentInitialDelay = 4;
     return request;
 }
 
-- (CGSize)getWindowSize
-{
+- (CGSize)getWindowSize {
     UIWindow *window = nil;
 
-    if (@available(iOS 13.0, *))
-    {
-        for (UIScene *scene in [UIApplication sharedApplication].connectedScenes)
-        {
-            if ([scene isKindOfClass:[UIWindowScene class]])
-            {
+    if (@available(iOS 13.0, *)) {
+        for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
+            if ([scene isKindOfClass:[UIWindowScene class]]) {
                 window = ((UIWindowScene *)scene).windows.firstObject;
                 break;
             }
         }
-    }
-    else
-    {
+    } else {
         window = [[UIApplication sharedApplication].delegate window];
     }
 
-    if (!window)
-        return CGSizeZero;
-
+    if (!window) return CGSizeZero;
+    
     UIEdgeInsets safeArea = UIEdgeInsetsZero;
     CGFloat screenScale = [UIScreen mainScreen].scale;
-    if (@available(iOS 11.0, *))
-    {
+    if (@available(iOS 11.0, *)) {
         safeArea = window.safeAreaInsets;
         safeArea.left /= screenScale;
         safeArea.bottom /= screenScale;
         safeArea.right /= screenScale;
     }
-
+    
     UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
     BOOL isLandscape = UIInterfaceOrientationIsLandscape(orientation);
-
+    
     CGSize size = CGSizeMake(window.bounds.size.width, window.bounds.size.height);
-
-    if (!isLandscape)
-    {
+    
+    if(!isLandscape){
         size.width -= safeArea.left + safeArea.right;
         size.height -= safeArea.top + safeArea.bottom;
     }
@@ -255,72 +229,76 @@ NSInteger const contentInitialDelay = 4;
     return size;
 }
 
-- (NSString *)resolutionJson
-{
-    // TODO: check why area is not clickable and safearea things
+- (NSString *)resolutionJson {
+    //TODO: check why area is not clickable and safearea things
     CGSize size = [self getWindowSize];
-
+    
     UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
     BOOL isLandscape = UIInterfaceOrientationIsLandscape(orientation);
 
     CGFloat lHpW = isLandscape ? size.height : size.width;
-    CGFloat lWpH = isLandscape ? size.width : size.height;
-
-    NSDictionary *resolutionDict = @{@"portrait" : @{@"height" : @(lWpH), @"width" : @(lHpW)}, @"landscape" : @{@"height" : @(lHpW), @"width" : @(lWpH)}};
-
+    CGFloat lWpH =  isLandscape ? size.width : size.height;
+    
+    NSDictionary *resolutionDict = @{
+        @"portrait": @{@"height": @(lWpH), @"width": @(lHpW)},
+        @"landscape": @{@"height": @(lHpW), @"width": @(lWpH)}
+    };
+    
     CLY_LOG_D(@"%s, %@", __FUNCTION__, resolutionDict);
-
+    
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:resolutionDict options:0 error:nil];
     return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
 }
 
-- (void)showContentWithHtmlPath:(NSString *)urlString placementCoordinates:(NSDictionary *)placementCoordinates
-{
+- (void)showContentWithHtmlPath:(NSString *)urlString placementCoordinates:(NSDictionary *)placementCoordinates {
     // Convert pathToHtml to NSURL
     NSURL *url = [NSURL URLWithString:urlString];
-
-    if (!url || !url.scheme || !url.host)
-    {
+    
+    if (!url || !url.scheme || !url.host) {
         CLY_LOG_E(@"%s, The URL is not valid: %@", __FUNCTION__, urlString);
         return;
     }
 
-    dispatch_async(dispatch_get_main_queue(), ^{
-        // Detect screen orientation
-        UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
-        BOOL isLandscape = UIInterfaceOrientationIsLandscape(orientation);
-
-        // Get the appropriate coordinates based on the orientation
-        NSDictionary *coordinates = isLandscape ? placementCoordinates[@"l"] : placementCoordinates[@"p"];
-
-        CGFloat x = [coordinates[@"x"] floatValue];
-        CGFloat y = [coordinates[@"y"] floatValue];
-        CGFloat width = [coordinates[@"w"] floatValue];
-        CGFloat height = [coordinates[@"h"] floatValue];
-
-        CGRect frame = CGRectMake(x, y, width, height);
-
-        // Log the URL and the frame
-        CLY_LOG_I(@"%s, Showing content from URL: %@", __FUNCTION__, url);
-        CLY_LOG_I(@"%s, Placement frame: %@", __FUNCTION__, NSStringFromCGRect(frame));
-
-        CountlyWebViewManager *webViewManager = CountlyWebViewManager.new;
-        [webViewManager createWebViewWithURL:url
-            frame:frame
-            appearBlock:^{
-                CLY_LOG_I(@"%s, Webview appeared", __FUNCTION__);
-                self->_isCurrentlyContentShown = YES;
-                [self clearContentState];
+    
+    dispatch_async(dispatch_get_main_queue(), ^ {
+    // Detect screen orientation
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    BOOL isLandscape = UIInterfaceOrientationIsLandscape(orientation);
+        
+    
+    // Get the appropriate coordinates based on the orientation
+    NSDictionary *coordinates = isLandscape ? placementCoordinates[@"l"] : placementCoordinates[@"p"];
+    
+    CGFloat x = [coordinates[@"x"] floatValue];
+    CGFloat y = [coordinates[@"y"] floatValue];
+    CGFloat width = [coordinates[@"w"] floatValue];
+    CGFloat height = [coordinates[@"h"] floatValue];
+    
+    CGRect frame = CGRectMake(x, y, width, height);
+    
+    // Log the URL and the frame
+    CLY_LOG_I(@"%s, Showing content from URL: %@", __FUNCTION__, url);
+    CLY_LOG_I(@"%s, Placement frame: %@", __FUNCTION__, NSStringFromCGRect(frame));
+    
+    CountlyWebViewManager* webViewManager =  CountlyWebViewManager.new;
+        [webViewManager createWebViewWithURL:url frame:frame appearBlock:^
+         {
+            CLY_LOG_I(@"%s, Webview appeared", __FUNCTION__);
+            self->_isCurrentlyContentShown = YES;
+            [self clearContentState];
+        } dismissBlock:^
+         {
+            CLY_LOG_I(@"%s, Webview dismissed", __FUNCTION__);
+            self->_isCurrentlyContentShown = NO;
+            self->_minuteTimer = [NSTimer scheduledTimerWithTimeInterval:self->_zoneTimerInterval
+                                                             target:self
+                                                           selector:@selector(enterContentZone)
+                                                           userInfo:nil
+                                                            repeats:NO];
+            if(self.contentCallback) {
+                self.contentCallback(CLOSED, NSDictionary.new);
             }
-            dismissBlock:^{
-                CLY_LOG_I(@"%s, Webview dismissed", __FUNCTION__);
-                self->_isCurrentlyContentShown = NO;
-                self->_minuteTimer = [NSTimer scheduledTimerWithTimeInterval:self->_zoneTimerInterval target:self selector:@selector(enterContentZone) userInfo:nil repeats:NO];
-                if (self.contentCallback)
-                {
-                    self.contentCallback(CLOSED, NSDictionary.new);
-                }
-            }];
+        }];
     });
 }
 #endif
