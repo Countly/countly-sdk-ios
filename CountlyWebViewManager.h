a77806a071343c51219366d39a3f88d289e0298c
Arif Burak Demiray <burakdemiray09@hotmail.com>
2026-01-21 00:42:50 +0300

    fix: linker os

--- CountlyWebViewManager.h
+++ CountlyWebViewManager.h
@@ -4,28 +4,18 @@
 //
 // Please visit www.count.ly for more information.
 
-#if (TARGET_OS_IOS)
-#import <UIKit/UIKit.h>
-#import <WebKit/WebKit.h>
+#if (TARGET_OS_IOS || TARGET_OS_IPHONE)
+  #import <UIKit/UIKit.h>
+  #import <WebKit/WebKit.h>
 #endif
 #import "CountlyCommon.h"
 
-#if (TARGET_OS_IOS)
-typedef NS_ENUM(NSUInteger, AnimationType) {
-    AnimationTypeSlideInFromBottom,
-    AnimationTypeSlideInFromTop,
-    AnimationTypeSlideInFromLeft,
-    AnimationTypeSlideInFromRight,
-    AnimationTypeIncreaseHeight,
-    AnimationTypeIncreaseHeightFromBottom
-};
+#if (TARGET_OS_IOS || TARGET_OS_IPHONE)
+typedef NS_ENUM(NSUInteger, AnimationType) { AnimationTypeSlideInFromBottom, AnimationTypeSlideInFromTop, AnimationTypeSlideInFromLeft, AnimationTypeSlideInFromRight, AnimationTypeIncreaseHeight, AnimationTypeIncreaseHeightFromBottom };
 
 @interface CountlyWebViewManager : NSObject <WKNavigationDelegate>
 
-- (void)createWebViewWithURL:(NSURL * _Nonnull)url
-                     frame:(CGRect)frame
-                 appearBlock:(void(^ __nullable)(void))appearBlock
-                dismissBlock:(void(^ __nullable)(void))dismissBlock;
+- (void)createWebViewWithURL:(NSURL *_Nonnull)url frame:(CGRect)frame appearBlock:(void (^__nullable)(void))appearBlock dismissBlock:(void (^__nullable)(void))dismissBlock;
 
 #else
 @interface CountlyWebViewManager : NSObject
