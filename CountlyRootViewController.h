19d47a3c48cf4e1590fcd2cc64cbb4e09cced993
Arif Burak Demiray <burakdemiray09@hotmail.com>
2026-01-20 13:31:51 +0300

    feat: custom root view controlelr for the view

---
+++ CountlyRootViewController.h
@@ -0,0 +1,12 @@
+// CountlyRootViewController.h
+//
+// This code is provided under the MIT License.
+//
+// Please visit www.count.ly for more information.
+
+#if (TARGET_OS_IOS)
+#import <UIKit/UIKit.h>
+
+@interface CountlyRootViewController : UIViewController
+@end
+#endif
